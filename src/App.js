577bb8bedc78990629a12df742d0532a6abfd383
JoshuaEsperilla <A01273743@itesm.mx>
2021-10-16 12:11:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
 
   const nombre = localStorage.getItem('miNombre');
+  const getNombre = () => {
+    try {
+        if (nombre === null) {
+            setName("Desconocido")
+            return [];
+        } else {
+            return JSON.parse(nombre);
+        }
+    } catch (err) {
+        return [];
+    }
+  };
 
-  const [name, setName] = useState(nombre);
+  const [name, setName] = useState(getNombre());
 
   function handleChange(e){
     setName(e.target.value);
-    localStorage.setItem('miNombre', JSON.stringify(e.target.value));
+    //Esta siguiente linea es para hacer string el valor ingresado pero ya que input es tipo texto y no estamos manejando una base de datos la página web
+    //no es susceptible a sqlInjection por lo que usar JSON.stringify no es necesario en este caso especifico
+    //localStorage.setItem('miNombre', (e.target.value));
   }
+
+  useEffect(() => {
+    document.title = `Benvenut@ ${name}`;
+    //Aun no tengo tan claro el uso de useEffect
+    //Me gustaria tener retro y saber cual es la mejor manera de hacerlo, aqui no lo convierte a string lo cual puede ser un problema si trataramos con una base de datos real
+  
+    localStorage.setItem('miNombre', JSON.stringify(name));
+
+  }, [name]);
 
   return (
     <div className="App">
@@ -20,10 +43,10 @@ function App() {
           <label htmlFor="myname">Nombre: </label>
           <input type="text" id="name" placeholder="Escribe tu nombre" onChange={handleChange}/>
           <p>
-            {name}
+            Hola {name}! :D
           </p>
           <h3>
-            Puedes recargar la página cuando quieras
+            Puedes recargar la página cuando quieras <em>x</em>
           </h3>
         </form>
       </header>
